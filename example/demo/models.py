b4041a5510c0f0c64be0c4c5721bc877c2bb871c
Andrey Kozhevnikov <coderusinbox@gmail.com>
2026-03-20 18:29:15 +0100

    5.0.0.76

--- example/demo/models.py
+++ example/demo/models.py
@@ -68,10 +68,9 @@ class FilesModel(models.Model):
 
     fs = OverwriteStorage()
 
-    compatible_choices_default = "5.0.0.72"
+    compatible_choices_default = "5.0.0.76"
     compatible_choices = (
-        ("5.0.0.72", "5.0.0.72"),
-        ("5.0.0.67", "5.0.0.67"),
+        ("5.0.0.76", "5.0.0.76"),
         ("4.6.0.15", "4.6.0.15"),
         ("4.5.0.25", "4.5.0.25"),
         ("4.4.0.72", "4.4.0.72"),
